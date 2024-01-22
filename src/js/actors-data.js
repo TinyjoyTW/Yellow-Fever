e0bb17a7143fc595f3f016705002b0313d9b7870
TinyjoyTW <152999162+TinyjoyTW@users.noreply.github.com>
2024-01-22 17:38:42 +0100

    finish actors table

---
+++ src/js/actors-data.js
@@ -0,0 +1,66 @@
+const actorsData = [
+  {
+    picture: "src/assets/pictures/VicentRodriguez1.jpg",
+    name: "Vincent Rodríguez III",
+    wikiURL: "https://en.wikipedia.org/wiki/Vincent_Rodriguez_III",
+    age: "41",
+    imdbURL: "https://www.imdb.com/name/nm4272259/?ref_=fn_al_nm_1",
+  },
+  {
+    picture: "src/assets/pictures/Awkwafina1.jpg",
+    name: "Awkwafina",
+    wikiURL: "https://es.wikipedia.org/wiki/Awkwafina",
+    age: "35",
+    imdbURL: "https://www.imdb.com/name/nm5377144",
+  },
+
+  {
+    picture: "src/assets/pictures/KiHongLee1.jpeg",
+    name: "Ki Hong Lee",
+    wikiURL: "https://en.wikipedia.org/wiki/Ki_Hong_Lee",
+    age: "37",
+    imdbURL: "https://www.imdb.com/name/nm3859624/",
+  },
+  {
+    picture: "src/assets/pictures/MannyJacinto1.jpeg",
+    name: "Manny Jacinto",
+    wikiURL: "https://es.wikipedia.org/wiki/Manny_Jacinto",
+    age: "36",
+    imdbURL: "https://www.imdb.com/name/nm3254274/",
+  },
+  {
+    picture: "src/assets/pictures/LucyLiu1.jpeg",
+    name: "Lucy Liu",
+    wikiURL: "https://es.wikipedia.org/wiki/Lucy_Liu",
+    age: "55",
+    imdbURL: "https://www.imdb.com/name/nm0005154/",
+  },
+  {
+    picture: "src/assets/pictures/JohnCho1.jpeg",
+    name: "John Cho",
+    wikiURL: "https://en.wikipedia.org/wiki/John_Cho",
+    age: "51",
+    imdbURL: "https://www.imdb.com/name/nm0158626/",
+  },
+  {
+    picture: "src/assets/pictures/ConstanceWu1.jpg",
+    name: "Constance Wu",
+    wikiURL: "https://en.wikipedia.org/wiki/Constance_Wu",
+    age: "41",
+    imdbURL: "https://www.imdb.com/name/nm2090422/",
+  },
+  {
+    picture: "src/assets/pictures/MichelleYeoh1.jpg",
+    name: "Michelle Yeoh",
+    wikiURL: "https://en.wikipedia.org/wiki/Michelle_Yeoh",
+    age: "61",
+    imdbURL: "https://www.imdb.com/name/nm0000706/",
+  },
+  {
+    picture: "src/assets/pictures/YayanRuhian1.jpeg",
+    name: "Yayan Ruhian",
+    wikiURL: "https://es.wikipedia.org/wiki/Yayan_Ruhian",
+    age: "55",
+    imdbURL: "https://www.imdb.com/name/nm3345670/",
+  },
+];
