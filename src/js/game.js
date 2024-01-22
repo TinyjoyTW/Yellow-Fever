73288b45c6909b2a9c47e316d581ddefaf432903
TinyjoyTW <152999162+TinyjoyTW@users.noreply.github.com>
2024-01-22 23:22:09 +0100

    create shuffle cards function and grid

--- src/js/game.js
+++ src/js/game.js
@@ -1,3 +1,14 @@
+// Helper functions placed on top:
+// Shuffling backwards is supposedly better than forward (???)
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+};
+
 class Game {
   constructor() {
     this.moveCount = 0;
@@ -6,22 +17,30 @@ class Game {
   }
 
   renderCards() {
-    // get the #cards-container element
+    // Get the #cards-container element
     const cardsContainer = document.getElementById("cards-container");
-    // iterate over the list of cards
-    // for each card we want to create two image elements
+    const list = [];
+    // Iterate over the list of cards
+    // For each card we want to create two image elements
     this.cards.forEach((card) => {
       const picture1 = document.createElement("img");
       const picture2 = document.createElement("img");
       picture1.setAttribute("src", card.picture1);
       picture2.setAttribute("src", card.picture2);
-      // append them to the cards-container
-      cardsContainer.appendChild(picture1);
-      cardsContainer.appendChild(picture2);
+      // Store created images into the list array
+      list.push(picture1, picture2);
+    });
+    
+    // Shuffle ths array with images we created above
+    shuffleArray(list);
+    // For each shuffled item in the array, we append it to the cardsContainer in HTML
+    list.forEach((l) => {
+      cardsContainer.appendChild(l);
     });
   }
 }
 
+// It waits for the HTML to finish loading before executing the code here
 window.onload = () => {
   new Game();
 };
